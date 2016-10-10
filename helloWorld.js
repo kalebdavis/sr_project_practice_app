5d992b941fd54a14a25abe6def88b033537fbd86
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-10-09 20:49:32 -0400

    remove hello world to make room for test app

--- helloWorld.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-  res.writeHead(200, {'content-Type': 'text/html' });
-  res.end('<h1>Hello world!</h1>');
-}).listen(3000);
-
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
